2e12506b276ea4214ff6e6bec8782fd455b267a5
Meital Avitan <meitala@berkeley.edu>
2020-06-06 20:11:00 +0300

    editing vs display mode on a todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Task from './Task'
-
-const dummy_tasks = [
-  {
-      id: 1, 
-      title: 'Pomodoro',
-      description: 'Try one round of the Pomodoro technique',
-      completed: true
-  },
-  {
-      id: 2,
-      title: 'Workout',
-      description: 'Do lower body/cardio workout',
-      completed: false
-  },
-    {
-      id: 3,
-      title: 'Start todo frontend- dummy tasks',
-      description: 'Design frontend with dummy tasks',
-      completed: true
-  },
-  {
-      id: 4,
-      title: 'Start todo frontend- tasts from DB',
-      description: 'Request from backend the actual tasks in DB',
-      completed: false
-  }
-]
+import axios from 'axios';
 
 function App() {
   const today = new Date()
   const formatted_today = today.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        '/api/todos',
+      );
+      setTasks(result.data);
+    };
+ 
+    fetchData();
+  }, []);
+
   return (
     <Modal.Dialog>
       <Modal.Header >
@@ -42,7 +29,7 @@ function App() {
 
       <Modal.Body>
         <ListGroup>
-          {dummy_tasks.map(task => 
+          {tasks.map(task => 
             <ListGroup.Item key={task.id}>
               <Task task={task}/>
             </ListGroup.Item>
@@ -51,8 +38,14 @@ function App() {
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="outline-primary">Clear all </Button>
-        <Button variant="outline-primary">+</Button>
+        <Button 
+          variant="outline-primary">
+          Clear all
+        </Button>
+        <Button 
+          variant="outline-primary">
+          +
+        </Button>
       </Modal.Footer>
   </Modal.Dialog>
   );
--- frontend/src/Task.js
+++ frontend/src/Task.js
@@ -1,44 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
+const Title = ({title, setTitle, editingTitle, setEditingTitle}) => {
+    return <div>{editingTitle ? <TitleInput title={title} setTitle={setTitle} setEditingTitle={setEditingTitle}/> 
+    : <div onClick={() => setEditingTitle(!editingTitle)}><TitleDisplay title={title}/></div>}</div>
+}
 
-const editIcon = <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
-<path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
-</svg>
+const TitleDisplay = ({title}) => (
+    <div>{title}</div>
+)
+
+// TODO: make title required (client-side validation)
+const TitleInput = ({title, setTitle, setEditingTitle}) => {
+    const inputElement = useRef(null)
+    useEffect(() => inputElement.current && inputElement.current.focus())
+    return <input 
+            required
+            ref={inputElement}
+            type='text' 
+            value={title}
+            style={{border: 'none', outline: 'none', borderBottom: '1px solid grey'}}
+            onChange = {(e) => setTitle(e.target.value)}
+            onBlur = {() => setEditingTitle(false)}
+        />
+    }
+
+const Description = ({description, setDescription, editingDescription, setEditingDescription}) => {
+     return <div>{editingDescription ? <DescriptionInput description={description} setDescription={setDescription} setEditingDescription={setEditingDescription}/> : <div onClick={() => setEditingDescription(!editingDescription)}> <DescriptionDisplay description={description}/> </div>} </div>
+}
+
+const DescriptionDisplay = ({description}) => (
+    <p style={{fontSize: '12px'}}>{description}</p>
+)
+
+const DescriptionInput = ({description, setDescription, setEditingDescription}) => {
+    const inputElement = useRef(null)
+    useEffect(() => inputElement.current && inputElement.current.focus())
+    return <input
+        ref={inputElement}
+        type='text' 
+        value={description}
+        style={{border: 'none', outline: 'none', borderBottom: '1px solid grey'}}
+        onChange = {(e) => setDescription(e.target.value)}
+        onBlur = {(e) => setEditingDescription(false)}
+    />
+}
 
 const Task = ({task}) => {
     const [completed, setCompleted] = useState(task.completed)
-    const [editing, setEditing] = useState(false)
+    const [title, setTitle] = useState(task.title)
+    const [description, setDescription] = useState(task.description)
+    const [editingTitle, setEditingTitle] = useState(false)
+    const [editingDescription, setEditingDescription] = useState(false)
+
     return (
-        <Form style={{'display': 'flex', 'align-items': 'center', 'justify-content': 'space-between'}}>
-            <Button 
-                style={{'margin-right': '18px'}} 
-                variant="outline-dark" 
-                size='sm'
-                onClick = {() => setEditing}
-                >
-                {editIcon}
-            </Button>
-            <span style={{'width': '70%'}}>
-                {task.title}
-                <p style={{'font-size': '12px'}}>{task.description}</p>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            <span style={{width: '70%'}}>
+                <Title title={title} setTitle={setTitle} editingTitle={editingTitle} setEditingTitle={setEditingTitle}/>
+                <Description description={description} setDescription={setDescription} editingDescription = {editingDescription} setEditingDescription={setEditingDescription}/>
             </span>
-            <span style={{'display': 'flex', 'align-items': 'center'}}>
-                <span >
+            <span style={{display: 'flex', alignItems: 'center'}}>
+                <span>
                 <Button 
-                    style={{'margin': '5px'}} 
-                    variant={completed ? 'success' : 'outline-info'} 
+                    style={{margin: '5px'}} 
+                    variant={completed ? 'success' : 'outline-success'} 
                     size='sm'
                     onClick={() => setCompleted(!completed)}
                     >
-                        {completed ?  '✓' : '__'}
+                    ✓
                 </Button> 
                 </span>
-                <Button style={{'margin': '5px'}} variant="outline-danger" size='sm'>X</Button>
+                <Button style={{margin: '5px'}} variant="outline-danger" size='sm'>✕</Button>
             </span>
-        </Form>
+        </div>
         )
     }
 
